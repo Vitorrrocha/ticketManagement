1d8426237a3038e10a43722fd9188fc689647b7b
vitorrrocha <44412471+Vitorrrocha@users.noreply.github.com>
2022-11-23 03:05:25 +0100

    Fix: adjst api response

--- ticket/api/routes.py
+++ ticket/api/routes.py
@@ -1,12 +1,14 @@
+from ticket.repo import TicketRepository
 
 def routes(app):
         @app.route('/')
         def index():
             return 'Index Page'
 
-        @app.route('/reedem/<ticketIdentifier>')
-        def hello():
-            return 'Hello, World'
+        @app.route('/reedem/<int:ticketIdentifier>', methods=['GET'])
+        def get_ticket(ticketIdentifier):
+            response = TicketRepository.get_ticket_by_id(ticketIdentifier)
+            return '{} {}'.format(response.status, response.message)
 
         @app.errorhandler(404)
         def error404(error):
