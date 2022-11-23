dbd12aa06cc2d1ddaeaa3204b540431a9d6aa60f
vitorrrocha <44412471+Vitorrrocha@users.noreply.github.com>
2022-11-23 19:20:18 +0100

    Feat: add more tests at use_case

--- ticket/use_case/get_ticket_test.py
+++ ticket/use_case/get_ticket_test.py
@@ -7,11 +7,24 @@ def test_get_ticket_with_ticket_not_redeemed_return_200():
     with patch('ticket.repo.ticket.TicketRepository.get_ticket_by_id') as get_ticket_by_id,\
         patch('ticket.repo.ticket.TicketRepository.update_ticket_by_id') as update_ticket_by_id:
 
-        id = 5
+        id = 1
         get_ticket_by_id.return_value = MagicMock(redeemed = False)
 
         response = GetTicket.get_ticket(id)
 
-        get_ticket_by_id.assert_called_once()
-        update_ticket_by_id.assert_called_once()
+        get_ticket_by_id.assert_called_once_with(1)
+        update_ticket_by_id.assert_called_once_with(1)
         assert response == {'status': 200, 'message': 'OK'}
+
+def test_get_ticket_with_ticket_redeemed_return_400():
+    with patch('ticket.repo.ticket.TicketRepository.get_ticket_by_id') as get_ticket_by_id,\
+        patch('ticket.repo.ticket.TicketRepository.update_ticket_by_id') as update_ticket_by_id:
+
+        id = 2
+        get_ticket_by_id.return_value = MagicMock(redeemed = True)
+
+        response = GetTicket.get_ticket(id)
+
+        get_ticket_by_id.assert_called_once_with(2)
+        update_ticket_by_id.assert_not_called()
+        assert response == {'status': 410, 'message': 'GONE'}
